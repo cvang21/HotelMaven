c64be5421cdd8316a6c9be2f94f57f14c091d578
cvadmin <cvadmin@cvpc>
2015-02-18 17:59:45 -0600

    Updated Commits

---
+++ HotelWeb/src/java/model/IHotelDAO.java
@@ -0,0 +1,32 @@
+package model;
+
+
+import entity.Hotel;
+import java.sql.SQLException;
+import java.util.List;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author cvadmin
+ */
+public interface IHotelDAO {
+
+    long deleteHotel(Hotel hotel) throws ClassNotFoundException, SQLException;
+
+    List<Hotel> getAllHotels() throws ClassNotFoundException, SQLException;
+
+    Hotel getHotelById(long hotelId) throws ClassNotFoundException, SQLException;
+
+    Hotel getHotelByName(String hotelName) throws ClassNotFoundException, SQLException;
+
+    long insertHotel(Hotel hotel) throws ClassNotFoundException, SQLException;
+
+    long updateHotel(Hotel hotel) throws ClassNotFoundException, SQLException;
+    
+}
